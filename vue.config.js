cadbc96cd9dc04b4beb65c1310fbbb0718f260d3
vinchantle <chenvincent610@gmail.com>
2022-02-20 10:05:46 +0000

    fourth commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js file to be placed in the root of your repository
-
+const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vinchantle.github.io/'
